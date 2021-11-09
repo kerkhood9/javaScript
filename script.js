379002bca68b65ca348cf70288498989435670c1
unknown <kerkhood9@gmail.com>
2021-11-09 16:10:31 +0200

    underheadline added

---
+++ script.js
@@ -0,0 +1,9 @@
+let names = ['hardy','khumbulani','william','Mmapaseka','happy','sithembiso','kagisho','mdud','precious','selina','nkosi','andiwe'];
+
+let display = document.getElementById("display");
+
+for (let i = 0; i < names.length; i++) {
+  display.innerHTML += "<hr>" + names[i] + "</hr>" 
+
+  }
+
